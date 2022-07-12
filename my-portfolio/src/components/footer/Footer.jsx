4ca5d6e1d93d1aad9c593e587249bb6cd44e7a5d
Winni731 <92243836+Winni731@users.noreply.github.com>
2022-07-12 13:19:26 -0400

    Update Footer.jsx

--- my-portfolio/src/components/footer/Footer.jsx
+++ my-portfolio/src/components/footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
+        <li><a href="#experience">Skills</a></li>
         <li><a href="#project">Project</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
